aec4d885cef83a49dadb4feff808a73934078287
Adiba Zehra <157864970+adiba-zehra@users.noreply.github.com>
2025-08-25 12:18:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -235,6 +235,4 @@ with gr.Blocks(title="AI Resume Analyzer", css=css) as demo:
     )
 
 if __name__ == "__main__":
-   demo.launch(server_name="0.0.0.0", server_port=7860, share=True)
-
-
+    demo.launch(share=True)
